331a4a25ff6af198f162927c013943f2ea6da010
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 07:52:19 +0100

    Add project files.

---
+++ DrawingBoardApp/DrawingBoard.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+using System;
+using System.Windows.Forms;
+
+namespace DrawingBoardApp
+{
+    class DrawingBoard
+    {
+        int tick;
+		private int scrWidth, scrHeight;
+        private bool y=false;
+        private bool x=false;
+        
+
+
+        
+        public DrawingBoard(MainForm mainForm)
+        {
+            scrWidth = mainForm.ClientSize.Width;
+            scrHeight = mainForm.ClientSize.Height;
+			tick = 0;
+        }
+
+        public void Draw(Graphics g)
+        {
+            // Clear the screen - for this frame
+            g.Clear(Color.FromArgb(228, 228, 228));
+
+            g.DrawEllipse(Pens.Red, 20+tick, 25+tick, 50, 50);
+        }
+
+        public void Tick()
+        {
+            if (x==true)
+            {
+                tick += 20;
+                if (tick >= scrWidth)
+                {
+                    x = false;
+                }
+            }
+            
+            if (x==false)
+            {
+                tick -= 20;
+                if (tick <= 0)
+                {
+                    x = true;
+                }
+            }
+            if (y == true)
+            {
+                tick += 20;
+                if (tick >= scrWidth)
+                {
+                    y = false;
+                }
+            }
+
+            if (y == false)
+            {
+                tick -= 20;
+                if (tick <= 0)
+                {
+                    y = true;
+                }
+            }
+
+
+            Console.WriteLine("Tick {0}", tick);
+        }
+    }
+}
---
+++ DrawingBoardApp/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DrawingBoardApp
+{
+    public partial class MainForm : Form
+    {
+        private readonly DrawingBoard drawingBoard;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.SetStyle(
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint,
+                true);
+            this.UpdateStyles();
+            drawingBoard = new(this);
+            AllocConsole();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        public void Draw(object sender, PaintEventArgs e)
+        {
+            // called by Paint
+            e.Graphics.SmoothingMode =
+                System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            drawingBoard.Draw(e.Graphics);
+        }
+
+        public void Refresh(object sender, EventArgs e)
+        {
+            // called by Timer Tick
+            drawingBoard.Tick();
+            this.Refresh();
+        }
+    }
+}
